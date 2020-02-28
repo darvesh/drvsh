99befb3f7254d161cb6867bd003b232140fed3c3
SitiSchu <admin@sitischu.com>
2020-02-28 02:25:21 +0100

    Highlight selected row
    Signed-off-by: SitiSchu <admin@sitischu.com>

--- render/components/highlight.js
+++ render/components/highlight.js
@@ -7,9 +7,9 @@ module.exports = (m, {content = '', language}) => {
                 .highlightAuto(((content || "") + "\n"), language)
                 .value
                 .split('\n')
-                .map((line, index) => m('tr', [
+                .map((line, index) => m('tr',{'id': `L${index}`, 'class': 'code-row'}, [
                     m('td.line-number',
-                        {'id': `L${index}`, 'data-line-number': index}),
+                        {'data-line-number': index}),
                     m('td', m.trust(`<code class="loc">${line}</code>`)),
                 ]))
         ),
