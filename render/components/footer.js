8c289f18d4a51fb44122f8158dbcd6e814931ba4
Fakruddin Darvesh A.K <darvesh31415@gmail.com>
2020-03-31 01:16:11 +0530

    [modify] footer info

--- render/components/footer.js
+++ render/components/footer.js
@@ -1,66 +1,68 @@
-module.exports = (m, {isSnippet}) => {
-    return m("footer", [
-        m("div.footer-keybindings",
-            m(
-                "a#newButton.submit",
-                {href: "/"},
-                [
-                    "New Snippet", m("span.keyboard-shortcut", [": Ctrl + Alt + A"])
-                ],
-            ),
-            "  ",
-            m(
-                "button#actionButton.submit",
-                {
-                    type: "submit",
-                    formaction: isSnippet ? "fork" : "/",
-                    formenctype: "application/x-www-form-urlencoded",
-                    formmethod: "post",
-                    form: "codeSnippet",
-                },
-                isSnippet
-                    ? ["Fork", m("span.keyboard-shortcut", [": Ctrl + Alt + F"])]
-                    : ["Save", m("span.keyboard-shortcut", [": Ctrl + S"])],
-            ),
-            " ",
-        ),
-        m("a#name-label", {
-            href: "https://github.com/mojurasu/memocho",
-            target: "blank",
-            rel: "noopener"
-        }, " Memochō"),
+module.exports = (m, { isSnippet }) => {
+	return m("footer", [
+		m(
+			"div.footer-keybindings",
+			m("a#newButton.submit", { href: "/" }, [
+				"New Snippet",
+				m("span.keyboard-shortcut", [": Ctrl + Alt + A"])
+			]),
+			"  ",
+			m(
+				"button#actionButton.submit",
+				{
+					type: "submit",
+					formaction: isSnippet ? "fork" : "/",
+					formenctype: "application/x-www-form-urlencoded",
+					formmethod: "post",
+					form: "codeSnippet"
+				},
+				isSnippet
+					? ["Fork", m("span.keyboard-shortcut", [": Ctrl + Alt + F"])]
+					: ["Save", m("span.keyboard-shortcut", [": Ctrl + S"])]
+			),
+			" "
+		),
+		m(
+			"a#name-label",
+			{
+				href: "https://github.com/solooo7",
+				target: "blank",
+				rel: "noopener"
+			},
+			" Hosted by Darvesh"
+		),
 
-        m("code.footer-text", [
-            " Built by",
-            m(
-                "a",
-                {
-                    href: "https://github.com/MKRhere",
-                    target: "blank",
-                    rel: "noopener"
-                },
-                "@MKRhere"
-            ),
-            ", Modified by",
-            m(
-                "a",
-                {
-                    href: "https://github.com/SitiSchu",
-                    target: "blank",
-                    rel: "noopener"
-                },
-                "@SitiSchu"
-            ),
-            "",
-            m(
-                "a",
-                {
-                    href: "https://github.com/mojurasu/memocho",
-                    target: "blank",
-                    rel: "noopener"
-                },
-                "Fork me!"
-            ),
-        ]),
-    ]);
+		m("code.footer-text", [
+			" Built by",
+			m(
+				"a",
+				{
+					href: "https://github.com/MKRhere",
+					target: "blank",
+					rel: "noopener"
+				},
+				"@MKRhere"
+			),
+			"&",
+			m(
+				"a",
+				{
+					href: "https://github.com/SitiSchu",
+					target: "blank",
+					rel: "noopener"
+				},
+				"@SitiSchu"
+			),
+			"",
+			m(
+				"a",
+				{
+					href: "https://github.com/solooo7/memocho",
+					target: "blank",
+					rel: "noopener"
+				},
+				"🍽️ Fork me!"
+			)
+		])
+	]);
 };
