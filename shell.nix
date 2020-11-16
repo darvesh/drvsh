a1a9db84e7ee06a787ec9e84f0988256c5f2a4d3
Darvesh <drvsh@protonmail.com>
2020-11-16 19:33:02 +0530

    remove: files

--- shell.nix
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env nix-shell
-with import <nixpkgs> {};
-stdenv.mkDerivation {
-    name = "mkrbin";
-    buildInputs = [ nodejs mongodb ];
-    BIN_MONGO_CSTRING=mongodb://localhost:27017/bin-store;
-    BIN_PORT=4000;
-}
-
